07db025e8e12413ccfd1d6a5a3a8da88a2f4092a
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-03-20 23:22:28 +0800

    feat(setup): routing management, providers, utils, design configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AdminLayout from "../components/AdminLayout";
+import VoterLayout from "../components/VoterLayout";
+import ProtectedRoute from "../components/ProtectedRoute";
+
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import Voters from "../pages/Voters";
+import Elections from "../pages/Elections";
+import Candidates from "../pages/Candidates";
+import VoterDashboard from "../pages/VoterDashboard"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+          <Route element={<AdminLayout />}>
+            <Route path="/dashboard/admin" element={<Dashboard />} />
+            <Route path="/dashboard/admin/elections" element={<Elections />} />
+            <Route path="/dashboard/admin/voters" element={<Voters />} />
+            <Route path="/dashboard/admin/candidates" element={<Candidates />} />
+          </Route>
+        </Route>
+
+        <Route element={<ProtectedRoute allowedRoles={["voter"]} />}>
+          <Route element={<VoterLayout />}>
+            <Route path="/dashboard/voter" element={<VoterDashboard />} />
+          </Route>
+        </Route>
+        
+        <Route path="/unauthorized" element={<div>Unauthorized</div>} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
