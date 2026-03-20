dbb045e075c9a161f2c138f2e37a0c7a80d9d02e
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-03-20 22:59:18 +0800

    refactor(auth): added update and delete voters endpoints

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,15 @@
 import express from "express"; 
 import { verifyToken } from '../middleware/verifyToken.js';
 import { authorizeRole } from '../middleware/authorizeRole.js';
-import { bulkSignup, login, signup } from "../controllers/authController.js";
+import { bulkSignup, deleteUser, login, signup, updateUser, getAllUsers } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", verifyToken, authorizeRole("admin"), signup);
 router.post("/bulk", verifyToken, authorizeRole("admin"), bulkSignup);
+router.put("/voter/:id", verifyToken, authorizeRole("admin"), updateUser);
+router.delete("/voter/:id", verifyToken, authorizeRole("admin"), deleteUser);
+router.get("/voters", verifyToken, authorizeRole("admin"), getAllUsers);
 
 export default router;
