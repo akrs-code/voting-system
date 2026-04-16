1f2c51006e9743aff559e293e3d7a57c8387a4f6
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-04-16 10:24:15 +0800

    refactor(models): indexing for optimized queries

--- backend/models/positionSchema.js
+++ backend/models/positionSchema.js
@@ -9,18 +9,21 @@ const positionSchema = new mongoose.Schema({
     type: String,
     enum: ["DIS", "DCS"],
   },
-  
+
   yearLevel: {
     type: Number,
     enum: [1, 2, 3, 4, null],
     default: null
   },
 
-  election: { 
+  election: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Election',
     required: true
-  }
+  },
+
 });
+
+positionSchema.index({ election: 1, department: 1 });
 
 export default mongoose.model("Position", positionSchema);
